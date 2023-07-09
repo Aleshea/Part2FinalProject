9d3ae3d2a35be8a96ae619823d38f305232ed9b8
Aleksa <60971552+Aleshea@users.noreply.github.com>
2023-07-09 18:45:44 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+TOKEN = '6350591168:AAHv5hZrifRD28Z5QOfde3BkIs_syOTz2wQ'
+
+keys = {
+    'биткоин': 'BTC',
+    'евро': 'EUR',
+    'доллар': 'USD',
+    'йена': 'JPY',
+    'юань': 'CNY',
+    'рубль': 'RUB',
+    'дирхам': 'AED',
+    'вона': 'KRW',
+    'тенге': 'KZT'
+}
